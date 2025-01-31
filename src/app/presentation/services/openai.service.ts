e6f1f72417692d80fb55cfd522529a2249c9de80
danielrubiointegramed <tu-correo@example.com>
2025-01-31 17:34:49 -0600

    Subiendo cambios

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { orthographyUseCase } from '@use-cases/index';
+import { orthographyUseCase, prosConsStreamUseCase, prosConsUseCase } from '@use-cases/index';
 import { from } from 'rxjs';
 
 
@@ -7,8 +7,17 @@ import { from } from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class OpenAiService {
 
-  checkOrthography( prompt: string ){
-    return from ( orthographyUseCase(prompt) );
+
+  checkOrthography( prompt: string ) {
+    return from( orthographyUseCase(prompt) ) ;
+  }
+
+  prosConsDiscusser( prompt: string ) {
+    return from( prosConsUseCase(prompt) );
+  }
+
+  prosConsStreamDiscusser( prompt: string, abortSignal: AbortSignal ) {
+    return prosConsStreamUseCase(prompt, abortSignal );
   }
 
 }
